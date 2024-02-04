6c76aa94713e248bca5670a11b4499189e6db9cf
Kim Lida <88290393+Kinukeo@users.noreply.github.com>
2024-02-04 09:45:38 +0000

    feat: created mock data service

---
+++ HololiveTalentsAPI/Services/MockLiveTalentService.cs
@@ -0,0 +1,20 @@
+﻿using HololiveTalentsApi.Models;
+using HololiveTalentsAPI.Controllers;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HololiveTalentsApi.Services
+{
+    public class MockLiveTalentService : ILiveTalentsService
+    {
+        public MockLiveTalentService()
+        {
+           
+        }
+
+        public Task<GetLiveTalentsModel> Get()
+        {
+             return Task.FromResult( new GetLiveTalentsModel { IsLive = true, LiveTalentsUrl = "dummyURL" });
+        }
+    }
+}
